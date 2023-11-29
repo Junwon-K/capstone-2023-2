22d08194f9376cecbc4e8a05bc77698b98394554
Junwon-K <71931665+Junwon-K@users.noreply.github.com>
2023-11-30 04:05:04 +0900

    heart.svg로 교체, 그 외 약간의 디자인 수정

--- kakaomap_test/js/recent_report.js
+++ kakaomap_test/js/recent_report.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', function () {
     ///////////////////////////////////////////////////////////////////////////
     function createDummyData() {
         return [
-            { id: 1, type: "장소명 오류", content: "주소가 정확하지 않습니다.", createDate: '2023-01-01', count: 5 },
+            { id: 1, type: "장애인 화장실 정보 수정", content: "남자 장애인 화장실이 있음", createDate: '2023-11-30', count: 5 },
             { id: 2, type: "정보 누락", content: "남자 화장실 기저귀 교환대가 없거나 파손됨.", createDate: '2023-02-15', count: 3 },
             { id: 3, type: "기타", content: "영업시간이 변경되었습니다.", createDate: '2023-03-20', count: 2 },
             { id: 1, type: "장소명 오류", content: "주소가 정확하지 않습니다.", createDate: '2023-01-01', count: 5 },
@@ -60,40 +60,65 @@ document.addEventListener('DOMContentLoaded', function () {
         </div>
         <div class="recent_likeButton">
             <div class="recent_likeButton_number">+ ${report.count}</div>
-            <div class="recent_likeButton_heart" data-report-id="${report.id}">♡</div>
+            <div class="recent_likeButton_heart" data-report-id="${report.id}">
+                <img src="/public/heart.svg" alt="♡">
+            </div>
         </div>
     
         `
-
-
-        const heartIcon = reportDiv.querySelector('.recent_likeButton_heart');
-        heartIcon.addEventListener('click', function (event) {
-            if (event.target.classList.contains('recent_likeButton_heart')) {
-                const reportId = event.target.getAttribute('data-report-id');
-                fetch(`/report/clickheart?id=${reportId}&placeid=${placeId}`)
-                    .then(response => {
-                        return response.text(); // Assuming the server responds with plain text
-                    })
-                    .then(body => {
-                        if (body.trim().toLowerCase() === "ip") {
-                            // Handle duplicated IP case
-                            alert('이미 신고 횟수가 추가 되었습니다.');
-                        }
-                        else if (body.trim().toLowerCase() === "count3") {
-                            alert('신고 누적으로 신고 내용이 반영되었습니다.');
-                            location.reload();
-                        } 
-                        else {
-                            // Handle successful submission	       
-                            alert('신고 횟수가 증가되었습니다.');
-                            location.reload();
-                        }
-                    });
-            }
-        });//리스너
+        const heartIcon = reportDiv.querySelector('.recent_likeButton_heart img');
+        heartIcon.addEventListener('click', function () {
+            const reportId = heartIcon.parentElement.getAttribute('data-report-id');
+            fetch(`/report/clickheart?id=${reportId}&placeid=${placeId}`)
+                .then(response => response.text())
+                .then(body => {
+                    if (body.trim().toLowerCase() === "ip") {
+                        // Handle duplicated IP case
+                        alert('이미 신고 횟수가 추가 되었습니다.');
+                    }
+                    else if (body.trim().toLowerCase() === "count3") {
+                        alert('신고 누적으로 신고 내용이 반영되었습니다.');
+                        location.reload();
+                    } 
+                    else {
+                        // Handle successful submission	       
+                        alert('신고 횟수가 증가되었습니다.');
+                        location.reload();
+                    }
+                });
+        });
 
         return reportDiv;
     }
+
+    // const heartIcon = reportDiv.querySelector('.recent_likeButton_heart');
+    //     heartIcon.addEventListener('click', function (event) {
+    //         if (event.target.classList.contains('recent_likeButton_heart')) {
+    //             const reportId = event.target.getAttribute('data-report-id');
+    //             fetch(`/report/clickheart?id=${reportId}&placeid=${placeId}`)
+    //                 .then(response => {
+    //                     return response.text(); // Assuming the server responds with plain text
+    //                 })
+    //                 .then(body => {
+    //                     if (body.trim().toLowerCase() === "ip") {
+    //                         // Handle duplicated IP case
+    //                         alert('이미 신고 횟수가 추가 되었습니다.');
+    //                     }
+    //                     else if (body.trim().toLowerCase() === "count3") {
+    //                         alert('신고 누적으로 신고 내용이 반영되었습니다.');
+    //                         location.reload();
+    //                     } 
+    //                     else {
+    //                         // Handle successful submission	       
+    //                         alert('신고 횟수가 증가되었습니다.');
+    //                         location.reload();
+    //                     }
+    //                 });
+    //         }
+    //     });
+
+    //     return reportDiv;
+    // }
 
     document.addEventListener("reportSubmitted", function () {
         fetchLatestReports();
